c391d9f02d842e98bc950629bab8475c82011d0a
fada <lukasz.czaplicki@gmail.com>
2014-07-05 19:25:42 +0200

    adapter with scroll

--- app/src/main/java/com/fada21/android/samplereaderlibraryscreen/adapter/CategoriesAdapter.java
+++ app/src/main/java/com/fada21/android/samplereaderlibraryscreen/adapter/CategoriesAdapter.java
@@ -1,5 +1,6 @@
 package com.fada21.android.samplereaderlibraryscreen.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,7 +26,7 @@ public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.Vi
     }
 
     public void setData(List<Category> categoriesList) {
-        this.categoriesList= categoriesList;
+        this.categoriesList = categoriesList;
         expandedMap = new boolean[getItemCount()];
         notifyDataSetChanged();
     }
@@ -45,7 +46,31 @@ public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.Vi
 
         String heading = viewHolder.categoryHeading.getContext().getResources().getString(R.string.category_name, name, booksList.size());
 
+        Context context = viewHolder.booksContainer.getContext();
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        int margin = (int) context.getResources().getDimension(R.dimen.space_m);
+
+        for (Book book : category.getBooksList()) {
+            View bookLayout = createBookLayout(book, layoutInflater, viewHolder.booksContainer);
+            setBookLayoutMargins(bookLayout, margin, margin);
+            viewHolder.booksContainer.addView(bookLayout);
+
+        }
+
         viewHolder.categoryHeading.setText(heading);
+    }
+
+    private View createBookLayout(Book book, LayoutInflater inflater, ViewGroup parent) {
+        View view = inflater.inflate(R.layout.vg_book, parent, false);
+        return view;
+    }
+
+    private void setBookLayoutMargins(View view, int leftMargin, int bottomMargin) {
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        if (layoutParams instanceof  ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams) layoutParams;
+            marginLayoutParams.setMargins(leftMargin, 0, 0, bottomMargin);
+        }
     }
 
     @Override
@@ -56,10 +81,12 @@ public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.Vi
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
         public TextView categoryHeading;
+        public LinearLayout booksContainer;
 
         public ViewHolder(View itemView) {
             super(itemView);
             categoryHeading = (TextView) itemView.findViewById(R.id.category_heading);
+            booksContainer = (LinearLayout) itemView.findViewById(R.id.books);
         }
     }
 }
